3b5a65cee8b246e763d2a12ca21130a894c30fc7
nguyenhuycong.dev@gmail.com <nguyenhuycong.dev@gmail.com>
2021-08-07 17:37:33 +0000

    add more allow host

--- saleor/settings.py
+++ saleor/settings.py
@@ -435,7 +435,7 @@ TEST_RUNNER = "saleor.tests.runner.PytestTestRunner"
 
 PLAYGROUND_ENABLED = get_bool_from_env("PLAYGROUND_ENABLED", True)
 
-ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,45.32.118.18"))
+ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,45.32.118.18,45.32.117.237"))
 ALLOWED_GRAPHQL_ORIGINS = get_list(os.environ.get("ALLOWED_GRAPHQL_ORIGINS", "*"))
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
